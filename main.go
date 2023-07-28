2e4f4765cb3580e0b3ba2c84ca9c986b12cf09bd
b0nz <elliantosarono.p@gmail.com>
2023-07-29 06:36:25 +0700

    update client & server code

--- main.go
+++ main.go
@@ -3,38 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-// type M map[string]interface{}
-
-// const MESSAGE_NEW_USER = "New User"
-// const MESSAGE_CHAT = "Chat"
-// const MESSAGE_LEAVE = "Leave"
-
-// var connections = make([]*WebSocketConnection, 0)
-
-// type SocketPayload struct {
-// 	Message string
-// }
-
-// type SocketResponse struct {
-// 	From    string
-// 	Type    string
-// 	Message string
-// }
-
-// type WebSocketConnection struct {
-// 	*websocket.Conn
-// 	Username string
-// }
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//
+		http.ServeFile(w, r, "./public/index.html")
 	})
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		// websocket
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println("Error upgrading connection:", err)
+			return
+		}
+
+		for {
+			messageType, message, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if err = conn.WriteMessage(messageType, message); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
 	})
 
 	fmt.Println("Server starting at :8080")
